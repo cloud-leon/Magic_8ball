4bd216420c5a339dc2e9ed67208299127c95916d
Joseph Aguiar <aguiarjo@bc.edu>
2022-07-18 20:07:13 +0000

    Basic backend setup

---
+++ GetAnswer.py
@@ -0,0 +1,24 @@
+import requests
+import pprint
+
+def is_question(question):
+    question_words = ['will', 'is', 'did', 'would']
+    for word in question_words:
+        if word not in str(question).lower():
+            has_question_word = False
+            break
+        has_question_word = True
+    if '?' not in str(question) or not has_question_word:
+        print("wrong")
+        return False
+    return True
+
+def get_answer(question):
+    if not is_question(question):
+        exit()
+    response = requests.get('https://8ball.delegator.com/magic/JSON/' + question)
+    data = response.json()
+    pprint.pprint(data)
+    return data['magic']
+
+get_answer('Will this work?')
